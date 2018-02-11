e25299da78892040ee412c7dbba8f120a83bdd14
Julia <berrylily.0709@gmail.com>
2018-02-11 13:14:05 -0800

    Apply material-ui

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from 'material-ui/Button';
+import { FormGroup } from 'material-ui/Form';
+import TextField from 'material-ui/TextField';
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+
 import './App.css';
 // import * as action from '../actions';
 
 class App extends Component {
   render() {
-    console.log(this.props);
     return (
       <div>
-        <header>
-          <h1>My todo list</h1>
-        </header>
-        <form>
-          <label>
-            Add new task:
-            <input type="text" name="task" />
-          </label>
-          <input type="submit" value="Add" onClick />
-        </form>
+        <Typography variant="display1" gutterBottom>
+          My todo list
+        </Typography>
+        <Grid item xs={12} sm={6}>
+          <FormGroup row>
+            <TextField id="task" label="New task" />
+            <Button variant="raised" color="primary">
+              Add
+            </Button>
+          </FormGroup>
+        </Grid>
       </div>
     );
   }
